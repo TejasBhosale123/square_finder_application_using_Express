ded36cdf55aeeb74dc69a215f73e9814f9fde89b
Tejas <tbhosale635@gmail.com>
2024-12-24 21:17:12 +0530

    Deployement

---
+++ sqrt_app/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Sqrt from './Sqrt'
+
+function App() {
+
+  return (
+    <>
+      <Sqrt/>
+    </>
+  )
+}
+
+export default App
---
+++ sqrt_app/src/Sqrt.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import {useState, useRef} from 'react';
+import axios from 'axios';
+
+const Sqrt = () => {
+
+    const rNum = useRef();
+    const [num, setNum] = useState("");
+    const [msg, setMsg] = useState("");
+    const hNum = (e) =>{
+        setNum(e.target.value);
+    }
+
+    const find = (e) =>{
+        e.preventDefault();
+
+        if(num === ""){
+            alert("Please Enter a number");
+            setMsg("")
+            rNum.current.focus();
+            return;
+        }
+
+        let url = 'http://localhost:3000/find';
+        let data = {params:{number:num}};
+
+        axios.get(url,data)
+        .then(res =>{
+            setMsg(res.data.msg);
+        })
+        .catch(err=>{
+            setMsg("Issue" +err)
+        })
+    }
+
+
+  return (
+    <div>
+        <center>
+            <h1> Square Root Finder Application Using Express and React</h1>
+            <form onSubmit={find}>
+                <input type='number' ref={rNum} onChange={hNum} step='any' placeholder='Enter a number'/>
+                <br/><br/>
+                <input type='submit' value='Find Square Root'/>
+            </form>
+            <h2> {msg} </h2>
+        </center>
+    </div>
+  )
+}
+
+export default Sqrt
